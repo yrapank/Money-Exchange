a9608c9f192780ac5722f8c6e076e85578792683
Yury Panamarou <yra.pank@gmail.com>
2018-09-24 16:36:04 +0300

    done!!

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    var money=currency;
+    var anser={};
+    if(currency<10000){
+        if(money-50>=0)anser.H=0;
+        for(var i=0;money-50>=0;i++){
+            anser.H++;
+            money-=50;
+            }
+        if(money-25>=0)anser.Q=0;
+        for(i=0;money-25>=0;i++){
+            anser.Q++;
+            money-=25;
+            }
+        if(money-10>=0)anser.D=0;
+        for(i=0;money-10>=0;i++){
+            anser.D++;
+            money-=10;
+            }
+        if(money-5>=0)anser.N=0;
+        for(i=0;money-5>=0;i++){
+            anser.N++;
+            money-=5;
+            }
+        if(money-1>=0)anser.P=0;
+        for(i=0;money-1>=0;i++){
+            anser.P++;
+            money-=1;
+            }
+            return anser;
+    }else return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+};
